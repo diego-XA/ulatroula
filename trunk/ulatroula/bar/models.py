4887db6de800b2071764e7cbfcefcf97916e4f4f
Diego <yuah.pich@gmail.com>
2012-05-09 12:50:46 +0200

    FEATURE
        * Engadidos modelos

--- trunk/ulatroula/bar/models.py
+++ trunk/ulatroula/bar/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Bar(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1024)
+    creation_date = models.DateTimeField()
+    votes = models.IntegerField()
+            
+class Event(models.Model):
+    bar = models.ForeignKey(Bar)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1024)
+    begin_date = models.DateTimeField()
+    end_date = models.DateTimeField()
